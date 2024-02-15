1f9f26adba51a054adefaf63b1711862ee1ca8c9
chithresh <chithreshgowda@gmail.com>
2024-02-15 21:22:59 +0530

    Registeration implemented

--- src/app/components/layout/Header.js
+++ src/app/components/layout/Header.js
@@ -1,20 +1,28 @@
 import Image from 'next/image';
 import Link from 'next/link';
- 
-export default function Header(){
-    return (
-        <header className='flex items-center justify-between'>
-        <Link className='text-primary font-semibold text-2xl' href=''>
-          {' '}
-          ST PiZZA
+
+export default function Header() {
+  return (
+    <header className='flex items-center justify-between'>
+      <Link className='text-primary font-semibold text-2xl' href=''>
+        {' '}
+        ST PiZZA
+      </Link>
+      <nav className='flex items-center gap-8 text-grey-500 font-semibold'>
+        <Link href={''}>Home</Link>
+        <Link href={''}>Menu</Link>
+        <Link href={''}>AboutUs</Link>
+        <Link href={''}>Contact</Link>
+      </nav>
+      <nav className='flex items-center gap-8 text-grey-500 font-semibold'>
+        <Link href={'/login'}>Login</Link>
+        <Link
+          className='bg-primary rounded-full text-white px-8 py-2'
+          href={'/register'}
+        >
+          Register
         </Link>
-        <nav className='flex items-center gap-8 text-grey-500 font-semibold'>
-          <Link href={''}>Home</Link>
-          <Link href={''}>Menu</Link>
-          <Link href={''}>AboutUs</Link>
-          <Link href={''}>Contact</Link>
-          <Link className="bg-primary rounded-full text-white px-8 py-2"href={''}>Login</Link>
-        </nav>
-      </header>
-    );
+      </nav>
+    </header>
+  );
 }
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import { Roboto } from 'next/font/google';
+import Header from './components/layout/Header';
 import './globals.css';
 
 const roboto = Roboto({ subsets: ['latin'], weight: ['400', '500', '700'] });
@@ -12,7 +13,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang='en'>
       <body className={roboto.className}>
-        <main className='max-w-4xl mx-auto p-4'>{children}</main>
+        <main className='max-w-4xl mx-auto p-4'>
+          <Header />
+          {children}
+        </main>
       </body>
     </html>
   );
